21af8e3d275b714c9eec7589e4d4a70ec45192bc
BuildTools <unconfigured@null.spigotmc.org>
2025-05-23 16:21:51 +0200

    Revert "barely works, but ok"
    This reverts commit c17d51a03aacad3b8c4ac66832c90c05eb885462.

--- src/main/java/com/hbm/tileentity/network/TileEntityCraneSplitter.java
+++ src/main/java/com/hbm/tileentity/network/TileEntityCraneSplitter.java
@@ -1,41 +1,31 @@
 package com.hbm.tileentity.network;
 
-import com.hbm.tileentity.TileEntityLoadedBase;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.network.NetworkManager;
-import net.minecraft.network.Packet;
-import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
+import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityCraneSplitter extends TileEntityLoadedBase {
+public class TileEntityCraneSplitter extends TileEntity {
 
 	/* false: left belt is preferred, true: right belt is preferred */
-	public int position = 0;
-	public int split = 2;
-	public boolean side = false;
-
-	@Override
-	public Packet getDescriptionPacket() {
-		NBTTagCompound nbt = new NBTTagCompound();
-		this.writeToNBT(nbt);
-		return new S35PacketUpdateTileEntity(this.xCoord, this.yCoord, this.zCoord, 0, nbt);
+	private boolean position;
+	
+	public void setPosition(boolean pos) {
+		this.position = pos;
+		this.worldObj.markTileEntityChunkModified(this.xCoord, this.yCoord, this.zCoord, this);
 	}
-
-	@Override
-	public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity pkt) {this.readFromNBT(pkt.func_148857_g());}
-
+	
+	public boolean getPosition() {
+		return this.position;
+	}
+	
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		super.readFromNBT(nbt);
-		this.position = nbt.getInteger("pos");
-		this.split = nbt.getInteger("split");
-		this.side = nbt.getBoolean("side");
+		this.position = nbt.getBoolean("pos");
 	}
-
+	
 	@Override
 	public void writeToNBT(NBTTagCompound nbt) {
 		super.writeToNBT(nbt);
-		nbt.setInteger("pos", this.position);
-		nbt.setInteger("split", this.split);
-		nbt.setBoolean("side", this.side);
+		nbt.setBoolean("pos", this.position);
 	}
 }
